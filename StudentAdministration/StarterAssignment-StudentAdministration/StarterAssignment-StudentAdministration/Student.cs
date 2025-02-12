af549c38dc862c67460a8ec33301ba9007e33fba
GijsvM <gijsvanmaanen006@gmail.com>
2025-02-12 19:09:19 +0100

    Program is functional now, Added ToDo list to keep track of everything that still has to be done.

--- StudentAdministration/StarterAssignment-StudentAdministration/StarterAssignment-StudentAdministration/Student.cs
+++ StudentAdministration/StarterAssignment-StudentAdministration/StarterAssignment-StudentAdministration/Student.cs
@@ -1,15 +1,16 @@
-namespace StudentAdinistration;
-
-public class Student()
+namespace StudentAdministration
 {
-    public string name { get; set; }
-    public string groupName { get; set; }
-    public int pcn { get; set; }
-
-    public Student(string name, string groupName, int pcn) : this()
+    public class Student
     {
-        this.name = name;
-        this.groupName = groupName;
-        this.pcn = pcn;
+        public string name { get; set; }
+        public string groupName { get; set; }
+        public int pcn { get; set; }
+
+        public Student(string name, string groupName, int pcn)
+        {
+            this.name = name;
+            this.groupName = groupName;
+            this.pcn = pcn;
+        }
     }
-}       
+}
